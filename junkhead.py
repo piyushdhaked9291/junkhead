73f6ad72f5ef14ce53c622b457b0b945c6496f50
piyushdhaked9291 <Piyush.11908666@lpu.in>
2022-01-16 21:57:36 +0530

    hey

--- junkhead.py
+++ junkhead.py
@@ -2,13 +2,21 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 
-def hello(update: Update, context: CallbackContext) -> None:
+def hello(update, context):
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+
+def myfile(update,context):
+    with open("BIO.txt", 'r') as f:
+        text = f.read()
+    update.message.reply_text(text)
+    
 
 updater = Updater('5033524993:AAG0QxeoeV-im4Wbb7HOHZZvLYhLaS_YZfs')
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('dev', myfile))
+
 
 updater.start_polling()
 updater.idle()
