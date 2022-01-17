a84b49d90b98d069602bbf32f658bcfdfd2eaee7
piyushdhaked9291 <Piyush.11908666@lpu.in>
2022-01-17 12:01:03 +0530

    updated command

--- junkhead.py
+++ junkhead.py
@@ -23,8 +23,8 @@ def main():
 
     updater.dispatcher.add_handler(CommandHandler('hello', hello))
     updater.dispatcher.add_handler(CommandHandler('dev', myfile))
-    updater.dispatcher.add_handler(CommandHandler('download', downloadcse423))
-    updater.dispatcher.add_handler(CommandHandler('download', downloadcse375))
+    updater.dispatcher.add_handler(CommandHandler('downloadcse423', downloadcse423))
+    updater.dispatcher.add_handler(CommandHandler('downloadcse375', downloadcse375))
 
 
     updater.start_polling()
