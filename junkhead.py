c61706687bef5ba5483b6c80de22fcfd771a5b81
piyushdhaked9291 <Piyush.11908666@lpu.in>
2022-01-19 10:54:23 +0530

    edfas

--- junkhead.py
+++ junkhead.py
@@ -17,6 +17,8 @@ def downloadcse423(update, context):
                              document=open("CSE423 cloud.pdf", 'rb'))
     context.bot.sendDocument(update.message.chat_id,
                              document=open("CSE423 VLAN.pdf", 'rb'))
+    context.bot.sendDocument(update.message.chat_id,
+                             document=open("CSE423 LEC-4.pptx"))
 
 
 def downloadcse375(update, context):
